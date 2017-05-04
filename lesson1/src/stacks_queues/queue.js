ecf005c7484e8961d3219d595e3580dbb693a04d
hruy28 <hruyw@yahoo.com>
2017-05-04 23:48:10 +0200

    solutions to data structure and algorithm challenge

--- lesson1/src/stacks_queues/queue.js
+++ lesson1/src/stacks_queues/queue.js
@@ -1,25 +1,29 @@
 class Queue {
 
   // todo: add a constructor? and implement the methods
+  constructor() {
+    this.queue = [];
+    this.top = 0;
+  }
 
   enqueue(value) {
-
+    return this.queue.push(value);
   }
 
   dequeue() {
-
+    return this.queue.shift();
   }
 
   peek() {
-
+    return this.queue[this.top];
   }
 
   length() {
-
+     return this.queue.length;
   }
 
   print() {
-
+    console.log(this.queue);
   }
 }
 module.exports = Queue;
