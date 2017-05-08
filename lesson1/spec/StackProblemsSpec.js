b275f18cad07c4868765c1851c1e7894f2291ee9
hruy28 <hruyw@yahoo.com>
2017-05-08 18:59:21 +0200

    changes in the stack spec file

--- lesson1/spec/StackProblemsSpec.js
+++ lesson1/spec/StackProblemsSpec.js
@@ -21,4 +21,16 @@ describe("StackProblems", function () {
     expect(StackProblems.balancedParenthesis("(hello) world)")).toBe(false);
     expect(StackProblems.balancedParenthesis("(hel((lo) w(or)ld)")).toBe(false);
   });
+
+  it ("should return correct postfix expression", function() {
+    expect(StackProblems.infixToPostfix("a+b*c")).toBe("abc*+");
+    expect(StackProblems.infixToPostfix("a+b/c*d")).toBe("abc/d*+");
+    expect(StackProblems.infixToPostfix("a-b-c*d/e+f^g")).toBe("abcde/*fg^+--");
+  });
+
+  it ("should evaluate the given postfix expression", function() {
+    expect(StackProblems.evaluatePostfix("abc*+")).toBe(37);
+    expect(StackProblems.evaluatePostfix("abc/d*+")).toBe(9.5);
+    expect(StackProblems.evaluatePostfix("abcde/*fg^+--")).toBe(89);
+  });
 });
