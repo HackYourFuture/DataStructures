1ce76c363e4f7578e80e2a51858e5321e4961596
Pablo <pablo.celentano@zivver.com>
2017-05-10 19:56:12 +0200

    new list problems + queue with stacks 🙌

--- lesson2/src/linked_list/list.js
+++ lesson2/src/linked_list/list.js
@@ -50,7 +50,7 @@ class LinkedList {
    Eg1: List(2, 4, 5, 4) size() -> 4
    */
   size() {
-    //todo: implement 🦑
+    return this.length;
   }
 
 
---
+++ lesson2/src/linked_list/queue.js
@@ -0,0 +1,23 @@
+let Stack = require("../../../lesson1/src/stacks_queues/stack");
+
+/*
+ This should be a queue implemented with 2 internal stacks !
+ */
+class StackQueue {
+  constructor() {
+    // create 2 stacks here... you are not allowed to use an array or list
+  }
+
+  enqueue(value) {
+    // implement all methods
+  }
+
+  dequeue() {
+    // implement all methods
+  }
+
+  peek() {
+    // implement all methods
+  }
+}
+module.exports = StackQueue;
