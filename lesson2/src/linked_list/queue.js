f84bd6290b475e7029513e1e41124769e2fd700c
hasanshahoud <hasansh343@gmail.com>
2017-05-13 19:42:47 +0200

    implemented queue using stacks..

--- lesson2/src/linked_list/queue.js
+++ lesson2/src/linked_list/queue.js
@@ -6,14 +6,21 @@ let Stack = require("../../../lesson1/src/stacks_queues/stack");
 class StackQueue {
   constructor() {
     // create 2 stacks here... you are not allowed to use an array or list
+    this.stack = new Stack();
+    this.stack1 = new Stack();
   }
-
+  
   enqueue(value) {
     // implement all methods
+    this.stack.push(value);
   }
 
   dequeue() {
     // implement all methods
+    while(!this.stack.isEmpty()){
+      this.stack1.push(this.stack.pop())
+    }
+    return this.stack1.pop();
   }
 
   peek() {
